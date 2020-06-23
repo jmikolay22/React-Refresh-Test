d61369a23e3ac804faa00b2d9729e736d90b4127
hk2rge <john.mikolay@siemens.com>
2020-06-23 11:11:08 -0400

    Add code for testing React-Refresh bug

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Dummy } from './Dummy';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Dummy val={'1. HMR Working '}></Dummy>
+        {Dummy( { val:'2. HMR Not Working ' } )}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ examples/create-react-app/src/Dummy.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export const Dummy = ( { val } ) => <div>{val}ReplaceMeToTest</div>;
+// Uncommenting any of the extra unused exports below
+// makes HMR work for both test cases in App.js
+//export const extraUnusedExport = ( { val } ) => <div>{val}123</div>;
+//export const extraUnusedExport = 'test';
+//export const extraUnusedExport = null;
+//export const extraUnusedExport = {};
